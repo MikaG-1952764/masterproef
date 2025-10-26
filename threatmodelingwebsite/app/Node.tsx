da06c764a619913c18a6829b19d28e1199c11f13
MikaGlkns <mika.gielkens01@gmail.com>
2025-10-26 23:42:16 +0100

    added some dummy data and gave each node a default width so the seperation is standardized

--- threatmodelingwebsite/app/Node.tsx
+++ threatmodelingwebsite/app/Node.tsx
@@ -2,13 +2,13 @@ export function Node({ name, level }: { name: string, level: string }) {
 
   if(level == "fortunate"){
       return (
-      <div className=" bg-green-600 rounded-[20] border border-black border-2 px-2 max-w-[160] text-center break-words py-1">
+      <div className=" bg-green-600 rounded-[20] border border-black border-2 px-2 w-[250] max-h-[200] text-center break-words py-1">
         {name}
       </div>
     );
   } else {
     return (
-      <div className=" bg-red-600 rounded-[20] border border-black border-2 px-2 max-w-[160] text-center break-words py-1">
+      <div className=" bg-red-600 rounded-[20] border border-black border-2 px-2 w-[250] max-h-[200] text-center break-words py-1">
         {name}
       </div>
     );
