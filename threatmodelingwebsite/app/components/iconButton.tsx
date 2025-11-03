638526f37598ba981dfbd2f016ec177d2df4d16e
MikaGlkns <mika.gielkens01@gmail.com>
2025-11-03 17:07:21 +0100

    changed icon settings

--- threatmodelingwebsite/app/components/iconButton.tsx
+++ threatmodelingwebsite/app/components/iconButton.tsx
@@ -4,7 +4,7 @@ import { TreeNode } from "../TreeVisualizer";
 
 export default function IconSelectorButton({treeNode, setTreeData, data} : {treeNode: TreeNode, setTreeData: (data: TreeNode) => void, data: TreeNode}) {
   const [open, setOpen] = useState(false);
-  const [selectedIcon, setSelectedIcon] = useState(<GoShield size={16} />);
+  const [selectedIcon, setSelectedIcon] = useState(treeNode.level==="fortunate" ? <GoShield size={16} color="orange"/> : <GoShield size={16} color="red"/>);
   const ref = useRef<HTMLDivElement>(null);
 
   // Close dropdown if clicking outside
@@ -18,9 +18,19 @@ export default function IconSelectorButton({treeNode, setTreeData, data} : {tree
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const icons = [
-    <GoShield size={16} color="black"/>,
-    <GoShieldCheck size={16} color="black"/>,
+  const iconsGreen = [
+    <GoShield size={16} color="orange"/>,
+    <GoShieldCheck size={16} color="green"/>,
+  ];
+
+  const iconsRedNoChild = [
+    <GoShield size={16} color="red"/>,
+    <GoShieldCheck size={16} color="orange"/>,
+  ];
+
+  const iconsRedChild = [
+    <GoShield size={16} color="red"/>,
+    <GoShieldCheck size={16} color="green"/>,
   ];
 
   return (
@@ -35,9 +45,50 @@ export default function IconSelectorButton({treeNode, setTreeData, data} : {tree
         {selectedIcon}
       </button>
 
-      {open && (
+      {open && treeNode.level==="fortunate" && (
         <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white border border-black rounded-full flex space-x-2 px-3 py-2 shadow-lg z-50">
-          {icons.map((icon, index) => (
+          {iconsGreen.map((icon, index) => (
+            <button
+              key={index}
+              className="hover:bg-red p-1 rounded-full"
+              onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedIcon(icon);
+                    treeNode.status=icon.type;
+                    setTreeData({...data});
+                    setOpen(false);
+                }}
+            >
+              {icon}
+            </button>
+          ))}
+        </div>
+      )}
+
+      {open && treeNode.level==="unfortunate" && !treeNode.children && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white border border-black rounded-full flex space-x-2 px-3 py-2 shadow-lg z-50">
+          {iconsRedNoChild.map((icon, index) => (
+            <button
+              key={index}
+              className="hover:bg-red p-1 rounded-full"
+              onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedIcon(icon);
+                    treeNode.status=icon.type;
+                    setTreeData({...data});
+                    setOpen(false);
+                }}
+            >
+              {icon}
+            </button>
+          ))}
+        </div>
+      )}
+
+
+      {open && treeNode.level==="unfortunate" && treeNode.children && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white border border-black rounded-full flex space-x-2 px-3 py-2 shadow-lg z-50">
+          {iconsRedChild.map((icon, index) => (
             <button
               key={index}
               className="hover:bg-red p-1 rounded-full"
